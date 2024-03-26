f6822da8d1ab2132299b0ae9d71fea37f6bc344f
Jorge Medina <jlmedinamacias@gmail.com>
2024-03-26 13:28:14 -0700

    eqArray function added to the file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,36 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let checker = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      checker++;
+    }
+  };
+  if (checker === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  let passed = `✅✅✅Assertion Passed: ${arr1} === ${arr2}`
+  let failed = `🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(passed);
+  } else {
+    console.log(failed);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3]);
+
+
+
+/*
 const assertArraysEqual = function (arr1, arr2) {
   let passed = `✅✅✅Assertion Passed: ${arr1} === ${arr2}`
   let failed = `🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`
@@ -20,3 +53,4 @@ const assertArraysEqual = function (arr1, arr2) {
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3]);
+*/
