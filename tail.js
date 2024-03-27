a2620148f37c35c56a2233892ed0c4b598234637
Jorge Medina <jlmedinamacias@gmail.com>
2024-03-27 12:52:54 -0700

    More test cases added

--- tail.js
+++ tail.js
@@ -21,10 +21,13 @@ console.log(una);
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
-
+const none = [];
 const empty = [1];
 console.log(tail(empty));
 
 assertEqual(tail(words).length, 2);
 assertEqual(tail(words)[0], "Lighthouse");
 assertEqual(tail(words)[1], "Labs");
+assertEqual(tail(none)[0], undefined);
+assertEqual(tail(empty)[0], undefined);
+assertEqual(words.length, 3);
