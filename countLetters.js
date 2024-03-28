ad162adfedeccb62487097ed83a2a64cbb69e852
Jorge Medina <jlmedinamacias@gmail.com>
2024-03-28 14:07:29 -0700

    Modified so it does not differentiate upper or lower case

--- countLetters.js
+++ countLetters.js
@@ -7,10 +7,31 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const countLetters = function (string) {
+/* const countLetters = function (string) {
   const count = {};
   for (const letter of string) {
-    if (letter == letter.toUpperCase()) {
+    console.log(letter);
+    let upperChar = letter.toUpperCase();
+    console.log(upperChar);
+        if (count[upperChar]) {
+          count[upperChar]++
+        } else if (!count[upperChar]) {
+          count[upperChar] = 1;
+        } else if (count[letter]) {
+          count[letter] += 1;
+        } else {
+          count[letter] = 1;
+        }
+  }
+  return count;
+}; */
+
+// countLetters("Lighthouse in the house");
+
+/* const countLetters = function (string) {
+  const count = {};
+  for (const letter of string) {
+    if (letter === letter.toUpperCase()) {
       if (count[letter]) {
         count[letter.toUpperCase()] += 1;
       } else {
@@ -25,20 +46,21 @@ const countLetters = function (string) {
       return count;
     };
   }
-};
-/*
+}; */
+
 const countLetters = function (string) {
   const count = {};
   for (const letter of string) {
-    if (count[letter]) {
-      count[letter] += 1;
+    let letterLow = letter.toLowerCase();
+    if (count[letterLow]) {
+      count[letterLow] += 1;
     } else {
-      count[letter] = 1;
+      count[letterLow] = 1;
     }
   }
   return count;
 };
-*/
+
 
 const countLettersObj = countLetters("lighthouse in the house");
 
@@ -50,11 +72,16 @@ assertEqual(countLettersObj["o"], 1);
 
 const countLettersObj2 = countLetters("Lighthouse in The House");
 
-assertEqual(countLettersObj2["i"], 2);
+assertEqual(countLettersObj["h"], 4);
+assertEqual(countLettersObj["i"], 2);
+assertEqual(countLettersObj["a"], 1);
+assertEqual(countLettersObj["w"], undefined);
+assertEqual(countLettersObj["o"], 1);
+/* assertEqual(countLettersObj2["i"], 2);
 assertEqual(countLettersObj2["h"], 3);
 assertEqual(countLettersObj2["H"], 1);
 assertEqual(countLettersObj2["a"], 1);
 assertEqual(countLettersObj2["w"], undefined);
 assertEqual(countLettersObj2["o"], 1);
 assertEqual(countLettersObj2["L"], 1);
-assertEqual(countLettersObj2["T"], 1);
+assertEqual(countLettersObj2["T"], 1); */
