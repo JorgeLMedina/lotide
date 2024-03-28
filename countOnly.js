e990eb6f2ac44de51626262156676ec3bf75b85a
Jorge Medina <jlmedinamacias@gmail.com>
2024-03-28 09:39:29 -0700

    File added

---
+++ countOnly.js
@@ -0,0 +1,53 @@
+//Function prints a message to the console indicating if given pair of primitive values are equal
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// Function reports back how many instances of each string were found in a given string array
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+  // let count = 0;
+  for (const item of allItems) {
+    // if (!results[item] && itemsToCount[item]) {
+    //   results[item] = 1;
+    // } else if (results[item]) {
+    //   results[item] += 1;
+    // }
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
