69492329b3ca7770d9017e16a970a6ccb287861d
Jorge Medina <jlmedinamacias@gmail.com>
2024-03-28 18:40:58 -0700

    File added

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  const keys = Object.keys(object);
+  for (const key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That's '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Breaking Bad"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Office"), undefined);
