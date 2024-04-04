8c420fc7d2c48d197a858a24dd34f7e112bd2e1c
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-04 13:39:47 -0700

    Refactoring accordingly with AI suggestions

--- eqObjects.js
+++ eqObjects.js
@@ -33,6 +33,33 @@ const eqObjects = function (object1, object2) {
   let countTrue = 0;
   for (const element of keys1) {
     if (Array.isArray(object1[element]) && Array.isArray(object2[element])) {
+      if (eqArrays(object1[element], object2[element]) === false) {
+        return false;
+      } else {
+        countTrue++;
+      }
+    } else if (object1[element] !== object2[element]) {
+      return false;
+    } else if (object1[element] === object2[element]) {
+      countTrue++;
+    }
+  }
+  if (countTrue < keys1.length) {
+    return false;
+  }
+  return true;
+};
+
+/* const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  let countTrue = 0;
+  for (const element of keys1) {
+    if (Array.isArray(object1[element]) && Array.isArray(object2[element])) {
       if (eqArrays(object1[element], object2[element]) === true) {
         countTrue++;
       } else {
@@ -46,7 +73,7 @@ const eqObjects = function (object1, object2) {
     return false;
   }
   return true;
-};
+}; */
 
 // Test 01
 const shirtObject = {
