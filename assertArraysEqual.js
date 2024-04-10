aaac1cb42c260c27e692d1a79630340b485b5dba
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-10 13:24:07 -0700

    Unnecessary blank spaces between lines removed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,6 @@
 const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (arr1, arr2) {
-
-
   if (eqArrays(arr1, arr2)) {
     let passed = `✅✅✅Assertion Passed: ${arr1} === ${arr2}`;
     console.log(passed);
