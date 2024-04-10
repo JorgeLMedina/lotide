55933423bbe7d6496d697e5c07184a2f733ba26a
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-09 20:18:20 -0700

    module.export and require() added and test cases moved to test file

--- head.js
+++ head.js
@@ -1,18 +1,8 @@
+const assertEqual = require("./assertEqual");
+
 // This function returns the first element of an array
 const head = function (arr) {
   return arr[0];
 };
 
-// This function compares 2 values and prints out if they are or not equivalent
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1, 2, 3, 4]), 1);
-assertEqual(head(["Yo"]), "Yo");
+module.exports = head;
