f195de40cbf6cc4a802490cec382a589c3f67d88
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-09 20:17:15 -0700

    module.export added and test cases moved to test file

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("identical", "identical");
-assertEqual(2, 5);
-assertEqual("not identical", "monkeyfuzz");
+module.exports = assertEqual;
