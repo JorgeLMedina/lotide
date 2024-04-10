77fc42aa5cd0bbc7ac59577342ce0bb6d23a1e23
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-10 13:00:29 -0700

    eqArrays refactored so only the main function stays in the file, test file created and test cases inserted in it. Proper connections between them made (module.exports - require)

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -23,30 +15,4 @@ const eqArrays = function (arr1, arr2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-/*
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-const eqArrays = function (arr1, arr2) {
-  let checker = 0;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      checker++;
-    }
-  };
-  if (arr1.length === arr2.length && checker === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-*/
+module.exports = eqArrays;
