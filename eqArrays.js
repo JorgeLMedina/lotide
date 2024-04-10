874c5f86792a32f48bf0cdac442d444550bd4e52
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-10 13:12:55 -0700

    assertArraysEqual refactored so only the main function stays in the file, test file created and test cases inserted in it. Proper connections between them made (module.exports - require)

--- eqArrays.js
+++ eqArrays.js
@@ -2,17 +2,12 @@ const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  let checker = 0;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      checker++;
+      return false;
     }
-  };
-  if (checker === 0) {
-    return true;
-  } else {
-    return false;
   }
+  return true;
 };
 
 module.exports = eqArrays;
