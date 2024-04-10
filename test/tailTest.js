dfb67121753563f528dab33e3c61a742300cfe46
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-10 11:55:42 -0700

    tailTest refactored so tail(words) and tail(empty) don't appear so much

--- test/tailTest.js
+++ test/tailTest.js
@@ -13,10 +13,13 @@ const none = [];
 const empty = [1];
 console.log(tail(empty));
 
-assertEqual(tail(words).length, 2);
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(tail(words)[1], "Labs");
-assertEqual(tail(none)[0], undefined);
-assertEqual(tail(empty)[0], undefined);
-assertEqual(tail(empty).length, 0);
+const wordsTail = tail(words);
+const emptyTail = tail(empty);
+
+assertEqual(wordsTail.length, 2);
+assertEqual(wordsTail[0], "Lighthouse");
+assertEqual(wordsTail[1], "Labs");
+assertEqual(tail(nemptyTailone)[0], undefined);
+assertEqual(emptyTail[0], undefined);
+assertEqual(emptyTail.length, 0);
 assertEqual(words.length, 3);
