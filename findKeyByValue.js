f20cb1c14b5bfa274be9cae9644d28958171e919
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-13 12:47:55 -0700

    Final adjustments to package.json and README created

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-/* const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-}; */
-
 const findKeyByValue = function (object, value) {
   const keys = Object.keys(object);
   for (const key of keys) {
@@ -16,15 +8,3 @@ const findKeyByValue = function (object, value) {
 };
 
 module.exports = findKeyByValue;
-/* const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That's '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Breaking Bad"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Office"), undefined); */
